6f0dbae710a02acd384701c38309fc897c608c6d
alfajriamali1933 <alfajriamali1933@gmail.com>
2024-10-10 12:04:28 +0700

    komit pertama

---
+++ src/constants/index.js
@@ -0,0 +1,82 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `Saya berprofesi sebagai Guru Produktif jurusan Rekayasa Perangkat Lunak (RPL) di salah satu SMK di
+Kota Bandung yang bernama SMK Medikacom. Saya mengajar sudah berjalan selama 2 tahun dengan mata pelajaran Pemrograman Web &
+Pemrograman Android. Saya juga berstatus sebagai Pegawai Tetap Yayasan dengan posisi Staf IT Sistem Informasi Manajemen (SIM)
+dengan jobdesk melakukan pengolahan data-data internal sekolah seperti data siswa, data pembayaran, data guru, data absensi,
+serta data kurikulum`;
+
+export const ABOUT_TEXT = `Lulusan S1 Program Studi Teknik Informatika, Universitas Komputer Indonesia. Berprofesi sebagai Guru & Pegawai Tetap Yayasan di SMK Medikacom, Kota Bandung. 
+Memiliki keterampilan dalam penguasaan di bidang Teknologi dan Informasi. Memiliki pengalaman organisasi mulai dari tingkat perguruan tinggi hingga tingkat daerah kepemudaan.
+Tekun & Fokus dalam melaksanakan tugas & tanggung jawab yang diberikan. Sedang mengajar di jurusan Rekayasa Perangkat Lunak (RPL)`;
+
+export const EXPERIENCES = [
+  {
+    year: "2022 - Present",
+    role: "Staf IT Sistem Informasi Manajemen (SIM)",
+    company: "SMK Medikacom",
+    description: `Melakukan Pengolahan Data Internal Sekolah & Melakukan Troubleshooting dan Maintenance Sistem Aplikasi Sekolah`,
+    technologies: ["PHP", "Native", "HTML", "MySQL"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Adobe",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Jl. Anatomi No.2 Komp. Dosen UNPAD, Cigadung, Cibeunying Kaler, Kota Bandung ",
+  phoneNo: "+62 812 9802 1258 ",
+  email: "rheyfauzan@gmail.com",
+};
